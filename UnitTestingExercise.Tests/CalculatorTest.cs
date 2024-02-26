8dac86dc864a48b475d5485ba300e5bbb8493297
Caris Williams <cwill1013@gmail.com>
2024-02-26 16:28:11 -0500

    Inital Commit

---
+++ UnitTestingExercise.Tests/CalculatorTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTestingExercise.Tests
+{
+    public class CalculatorTests0
+    {
+        [Theory]
+        [InlineData(0, 1, 1, 2)]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(-1, -1, -1, -3)]
+        [InlineData(-2, 1, 1, 0)]
+        public void Add_Test(int num1, int num2, int num3, int expected)
+        {
+            var c = new Calculator();
+
+            var actual = c.Add(num1, num2, num3);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0, 1, -1)]
+        [InlineData(0, 0, 0)]
+        [InlineData(-1, -1, 0)]
+        [InlineData(-2, 1, -3)]
+        public void Subtract_Test(int num1, int num2, int expected)
+        {
+            var c = new Calculator();
+
+            var actual = c.Subtract(num1, num2);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0, 1, 0)]
+        [InlineData(0, 0, 0)]
+        [InlineData(3, 3, 9)]
+        [InlineData(5, 5, 25)]
+        public void Multiply_Test(int num1, int num2, int expected)
+        {
+            var c = new Calculator();
+
+            var actual = c.Multiply(num1, num2);
+
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(2, 1, 2)]
+        [InlineData(0, 0, 0)]
+        [InlineData(3, 3, 1)]
+        [InlineData(40, 8, 5)]
+        public void Divide_Test(int num1, int num2, int expected)
+        {
+            var c = new Calculator();
+
+            var actual = c.Divide(num1, num2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using Xunit;
 
 namespace UnitTestingExercise.Tests
@@ -13,54 +14,57 @@ namespace UnitTestingExercise.Tests
 
             //Arrange
             // create a Calculator object
-            
+           Calculator instance = new Calculator();
 
             //Act
                 // call the Add method that is located in the Calculator class
                 // and store its result in a variable named actual
-
+                int actual = instance.Add(num1, num2, num3);
             //Assert
                 //Assert.Equal(expected, actual);
+                Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(10,5,5)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
 
             //Arrange
-
+            Calculator instance = new Calculator();
             //Act
-
+            int actual = instance.Subtract(minuend, subtrhend);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(5, 5, 25)]//Add test data <-------
+        [InlineData(2, 5, 10)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
 
             //Arrange
-
+            Calculator instance = new Calculator();
             //Act
-
+           int actual = instance.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]//Add test data <-------
+        [InlineData(10, 5, 2)]//Add test data <-------
+        [InlineData(20, 5, 4)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
-
+            Calculator instance = new Calculator();
             //Act
-
+            int actual = instance.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
     }
